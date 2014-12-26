2f5064f732c8006135ef80f97247c8d754b5e1b3
Scott Ellis <scott@jumpnowtek.com>
2014-12-26 16:18:02 -0500

    usbhost: remove pandaboard hw init and use fdt/usb driver for gpio reset

--- sys/arm/ti/omap4/duovero/duovero.c
+++ sys/arm/ti/omap4/duovero/duovero.c
@@ -26,7 +26,9 @@
  * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
  * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  *
- * Taken from head/sys/arm/ti/omap4/pandaboard/pandaboard.c
+ * Modified from head/sys/arm/ti/omap4/pandaboard/pandaboard.c
+ * We only need the AUXCLK3 activation piece. The rest is handled by the
+ * usb host driver and FDT settings and u-boot gpio pin muxing.
  */
 #include <sys/cdefs.h>
 __FBSDID("$FreeBSD$");
@@ -48,30 +50,9 @@ __FBSDID("$FreeBSD$");
 #include <arm/ti/omap4/omap4_reg.h>
 
 /* Registers in the SCRM that control the AUX clocks */
-#define SCRM_ALTCLKSRC			     (0x110)
-#define SCRM_AUXCLK0                         (0x0310)
-#define SCRM_AUXCLK1                         (0x0314)
-#define SCRM_AUXCLK2                         (0x0318)
 #define SCRM_AUXCLK3                         (0x031C)
 
-/* Some of the GPIO register set */
-#define GPIO1_OE                             (0x0134)
-#define GPIO1_CLEARDATAOUT                   (0x0190)
-#define GPIO1_SETDATAOUT                     (0x0194)
-#define GPIO2_OE                             (0x0134)
-#define GPIO2_CLEARDATAOUT                   (0x0190)
-#define GPIO2_SETDATAOUT                     (0x0194)
-
-/* Some of the PADCONF register set */
-#define CONTROL_WKUP_PAD0_FREF_CLK3_OUT  (0x058)
-#define CONTROL_CORE_PAD1_KPD_COL2       (0x186)
-#define CONTROL_CORE_PAD0_GPMC_WAIT1     (0x08C)
-
 #define REG_WRITE32(r, x)    *((volatile uint32_t*)(r)) = (uint32_t)(x)
-#define REG_READ32(r)        *((volatile uint32_t*)(r))
-
-#define REG_WRITE16(r, x)    *((volatile uint16_t*)(r)) = (uint16_t)(x)
-#define REG_READ16(r)        *((volatile uint16_t*)(r))
 
 /**
  *	usb_hub_init - initialises and resets the external USB hub
@@ -81,36 +62,27 @@ __FBSDID("$FreeBSD$");
  *	layout of the USB hub taken from the Pandaboard reference manual.
  *
  *
- *	   .-------------.         .--------------.         .----------------.
- *	   |  OMAP4430   |         |   USB3320C   |         |    LAN9514     |
- *	   |             |         |              |         | USB Hub / Eth  |
- *	   |         CLK | <------ | CLKOUT       |         |                |
- *	   |         STP | ------> | STP          |         |                |
- *	   |         DIR | <------ | DIR          |         |                |
- *	   |         NXT | <------ | NXT          |         |                |
- *	   |        DAT0 | <-----> | DAT0         |         |                |
- *	   |        DAT1 | <-----> | DAT1      DP | <-----> | DP             |
- *	   |        DAT2 | <-----> | DAT2      DM | <-----> | DM             |
- *	   |        DAT3 | <-----> | DAT3         |         |                |
- *	   |        DAT4 | <-----> | DAT4         |         |                |
- *	   |        DAT5 | <-----> | DAT5         |  +----> | N_RESET        |
- *	   |        DAT6 | <-----> | DAT6         |  |      |                |
- *	   |        DAT7 | <-----> | DAT7         |  |      |                |
- *	   |             |         |              |  |  +-> | VDD33IO        |
- *	   |    AUX_CLK3 | ------> | REFCLK       |  |  +-> | VDD33A         |
- *	   |             |         |              |  |  |   |                |
- *	   |     GPIO_62 | --+---> | RESET        |  |  |   |                |
- *	   |             |   |     |              |  |  |   |                |
- *	   |             |   |     '--------------'  |  |   '----------------'
- *	   |             |   |     .--------------.  |  |
- *	   |             |   '---->| VOLT CONVERT |--'  |
- *	   |             |         '--------------'     |
- *	   |             |                              |
- *	   |             |         .--------------.     |
- *	   |      GPIO_1 | ------> |   TPS73633   |-----'
- *	   |             |         '--------------'
- *	   '-------------'
- *	
+ *	   .-------------.         .--------------.
+ *	   |  OMAP4430   |         |   USB3326    |
+ *	   |             |         |              |
+ *	   |         CLK | <------ | CLKOUT       |
+ *	   |         STP | ------> | STP          |
+ *	   |         DIR | <------ | DIR          |
+ *	   |         NXT | <------ | NXT          |
+ *	   |        DAT0 | <-----> | DAT0         |
+ *	   |        DAT1 | <-----> | DAT1      DP |
+ *	   |        DAT2 | <-----> | DAT2      DM |
+ *	   |        DAT3 | <-----> | DAT3         |
+ *	   |        DAT4 | <-----> | DAT4         |
+ *	   |        DAT5 | <-----> | DAT5         |
+ *	   |        DAT6 | <-----> | DAT6         |
+ *	   |        DAT7 | <-----> | DAT7         |
+ *	   |             |         |              |
+ *	   |    AUX_CLK3 | ------> | REFCLK       |
+ *	   |             |         |              |
+ *	   |     GPIO_62 | ------> | RESET        |
+ *	   |             |         |              |
+ *	   '-------------'         '--------------'
  *
  *	RETURNS:
  *	nothing.
@@ -118,60 +90,22 @@ __FBSDID("$FreeBSD$");
 static void
 usb_hub_init(void)
 {
-	bus_space_handle_t scrm_addr, gpio1_addr, gpio2_addr, scm_addr;
+	bus_space_handle_t scrm_addr;
 
 	if (bus_space_map(fdtbus_bs_tag, OMAP44XX_SCRM_HWBASE,
-	    OMAP44XX_SCRM_SIZE, 0, &scrm_addr) != 0)
+	                  OMAP44XX_SCRM_SIZE, 0, &scrm_addr) != 0)
 		panic("Couldn't map SCRM registers");
-	if (bus_space_map(fdtbus_bs_tag, OMAP44XX_GPIO1_HWBASE, 
-	    OMAP44XX_GPIO1_SIZE, 0, &gpio1_addr) != 0)
-		panic("Couldn't map GPIO1 registers");
-	if (bus_space_map(fdtbus_bs_tag, OMAP44XX_GPIO2_HWBASE,
-	    OMAP44XX_GPIO2_SIZE, 0, &gpio2_addr) != 0)
-		panic("Couldn't map GPIO2 registers");
-	if (bus_space_map(fdtbus_bs_tag, OMAP44XX_SCM_PADCONF_HWBASE,
-	    OMAP44XX_SCM_PADCONF_SIZE, 0, &scm_addr) != 0)
-		panic("Couldn't map SCM Padconf registers");
-
-	
 
 	/* Need to set FREF_CLK3_OUT to 19.2 MHz and pump it out on pin GPIO_WK31.
 	 * We know the SYS_CLK is 38.4Mhz and therefore to get the needed 19.2Mhz,
 	 * just use a 2x divider and ensure the SYS_CLK is used as the source.
 	 */
-	REG_WRITE32(scrm_addr + SCRM_AUXCLK3, (1 << 16) |    /* Divider of 2 */
+	REG_WRITE32(scrm_addr + SCRM_AUXCLK3,
+	                          (1 << 16) |    /* Divider of 2 */
 	                          (0 << 1) |     /* Use the SYS_CLK as the source */
 	                          (1 << 8));     /* Enable the clock */
 
-	/* Enable the clock out to the pin (GPIO_WK31). 
-	 *   muxmode=fref_clk3_out, pullup/down=disabled, input buffer=disabled,
-	 *   wakeup=disabled.
-	 */
-	REG_WRITE16(scm_addr + CONTROL_WKUP_PAD0_FREF_CLK3_OUT, 0x0000);
-
-
-	/* Disable the power to the USB hub, drive GPIO1 low */
-	REG_WRITE32(gpio1_addr + GPIO1_OE, REG_READ32(gpio1_addr + 
-	    GPIO1_OE) & ~(1UL << 1));
-	REG_WRITE32(gpio1_addr + GPIO1_CLEARDATAOUT, (1UL << 1));
-	REG_WRITE16(scm_addr + CONTROL_CORE_PAD1_KPD_COL2, 0x0003);
-	
-	
-	/* Reset the USB PHY and Hub using GPIO_62 */
-	REG_WRITE32(gpio2_addr + GPIO2_OE, 
-	    REG_READ32(gpio2_addr + GPIO2_OE) & ~(1UL << 30));
-	REG_WRITE32(gpio2_addr + GPIO2_CLEARDATAOUT, (1UL << 30));
-	REG_WRITE16(scm_addr + CONTROL_CORE_PAD0_GPMC_WAIT1, 0x0003);
-	DELAY(10);
-	REG_WRITE32(gpio2_addr + GPIO2_SETDATAOUT, (1UL << 30));
-
-	
-	/* Enable power to the hub (GPIO_1) */
-	REG_WRITE32(gpio1_addr + GPIO1_SETDATAOUT, (1UL << 1));
 	bus_space_unmap(fdtbus_bs_tag, scrm_addr, OMAP44XX_SCRM_SIZE);
-	bus_space_unmap(fdtbus_bs_tag, gpio1_addr, OMAP44XX_GPIO1_SIZE);
-	bus_space_unmap(fdtbus_bs_tag, gpio2_addr, OMAP44XX_GPIO2_SIZE);
-	bus_space_unmap(fdtbus_bs_tag, scm_addr, OMAP44XX_SCM_PADCONF_SIZE);
 }
 
 /**
@@ -188,33 +122,13 @@ usb_hub_init(void)
  *
  *	What we do have:
  *	   - virt/phys mappings from the devmap (see omap4.c)
- *	   - 
- *
- *
- *	So we are hamstrung without the useful drivers and we have to go back to
- *	direct register manupulation. Luckly we don't have to do to much, basically
- *	just setup the usb hub/ethernet.
- *
- *      The duovero doesn't have built-in usb/ethernet the way the pandaboard does.
- *      We still want the USB Host initialized so we can use a USB wifi dongle.
- *      This code might be doing more then is necessary for the duovero.
  *
  */
 static void
 board_init(void *dummy)
 {
-	/* Initialise the USB phy and hub */
+	/* Initialise the USB phy ref clock */
 	usb_hub_init();
-	
-	/*
-	 * XXX Board identification e.g. read out from FPGA or similar should
-	 * go here
-	 * We could read the EEPROM to get the Parlor board ID. But that would
-	 * require an I2C driver, so maybe not here if the above notes are correct.
-	 * U-Boot already identified and did some board setup for us (LAN/GPMC,
-	 * onboard WIFI), so maybe not necessary. Not that we can use either
-	 * until we have drivers...
-	 */
 }
 
 SYSINIT(board_init, SI_SUB_CPU, SI_ORDER_THIRD, board_init, NULL);
