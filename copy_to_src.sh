f20782200c82770c34e3723ec730f25caae0acd2
Scott Ellis <scott@jumpnowtek.com>
2014-12-31 09:09:26 -0500

    copy script: Add reset patches

--- copy_to_src.sh
+++ copy_to_src.sh
@@ -10,4 +10,6 @@ patch -p1 -i ${GITDIR}/patches/0001-omap4-add-aux-clks-to-prcm.patch
 patch -p1 -i ${GITDIR}/patches/0002-ti-ehci-add-support-for-2-gpio-reset-pins.patch
 patch -p1 -i ${GITDIR}/patches/0003-pandaboard-usb-let-ehci-driver-initialize-usb-phy.patch
 patch -p1 -i ${GITDIR}/patches/0004-add-duovero-support.patch
+patch -p1 -i ${GITDIR}/patches/0005-omap4-prcm-use-defined-constants-in-reset-function.patch
+patch -p1 -i ${GITDIR}/patches/0006-omap4-do-warm-reset-on-reboot.patch
 
