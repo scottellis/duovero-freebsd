b6521e12653d6335fb79fecfcf06b54c2344332e
Scott Ellis <scott@jumpnowtek.com>
2015-01-14 16:48:24 -0500

    Converting omap443x.dtsi for Linux compat

--- copy_to_src.sh
+++ copy_to_src.sh
@@ -13,4 +13,7 @@ patch -p0 -i ${GITDIR}/patches/0004-add-duovero-support.patch
 patch -p0 -i ${GITDIR}/patches/0005-omap4-prcm-use-defined-constants-in-reset-function.patch
 patch -p0 -i ${GITDIR}/patches/0006-omap4-do-warm-reset-on-reboot.patch
 patch -p0 -i ${GITDIR}/patches/0007-omap4-add-omap443x-dtsi.patch
+patch -p0 -i ${GITDIR}/patches/0008-dts-omap443x-remove-unused-address-cells-prop.patch
+patch -p0 -i ${GITDIR}/patches/0009-dts-omap443x-move-cache-controller-up-one-level.patch
+patch -p0 -i ${GITDIR}/patches/0010-dts-omap443x-remove-redundant-interrupt-parent-prop.patch
 
