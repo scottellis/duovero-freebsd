55ed17e8018acdbc31bc74b9714f727215e98867
Scott Ellis <scott@jumpnowtek.com>
2015-01-23 16:23:35 -0500

    Broke omap443x patch into two, add then use

--- copy_to_src.sh
+++ copy_to_src.sh
@@ -6,6 +6,7 @@ GITDIR=`pwd`
 
 cd $FREEBSD_SRC
 
+patch -p0 -i ${GITDIR}/patches/0000-omap4-prcm-fix-uart-clock-reg-address.patch
 patch -p0 -i ${GITDIR}/patches/0001-omap4-add-aux-clks-to-prcm.patch
 patch -p0 -i ${GITDIR}/patches/0002-ti-ehci-add-support-for-2-gpio-reset-pins.patch
 patch -p0 -i ${GITDIR}/patches/0003-pandaboard-usb-let-ehci-driver-initialize-usb-phy.patch
@@ -13,4 +14,7 @@ patch -p0 -i ${GITDIR}/patches/0004-add-duovero-support.patch
 patch -p0 -i ${GITDIR}/patches/0005-omap4-prcm-use-defined-constants-in-reset-function.patch
 patch -p0 -i ${GITDIR}/patches/0006-omap4-do-warm-reset-on-reboot.patch
 patch -p0 -i ${GITDIR}/patches/0007-omap4-add-omap443x-dtsi.patch
+patch -p0 -i ${GITDIR}/patches/0008-omap4-duovero-pandaboard-use-omap443x-dtsi.patch
+
+#patch -p0 -i ${GITDIR}/patches/0010-add-kernconf-for-bbb.patch
 
